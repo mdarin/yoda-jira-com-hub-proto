f79e517446ffe696183d018c4d08e4542fff86f3
m.darin <m.darin.comco@yandex.ru>
2020-09-01 20:09:12 +0300

    move email sending to func

--- main.go
+++ main.go
@@ -43,6 +43,26 @@ func handle_hi(w http.ResponseWriter, r *http.Request) {
 }
 
 func handle_world(w http.ResponseWriter, r *http.Request) {
+
+	// get body
+	body, _ := ioutil.ReadAll(r.Body)
+
+	log.Println(string(body))
+
+	send_email(string(body))
+
+	fmt.Fprintf(w, "[world] webhook machined!")
+}
+
+func handle_jira_webhook(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "webhook machined!")
+}
+
+//-------------------------------
+// internals
+//-------------------------------
+func send_email(message string) {
 	// yandex
 	// smtp.yandex.ru:465
 	// http://ilyakhasanov.ru/baza-znanij/prochee/nuzhno-znat/139-nastrojki-otpravki-pochty-cherez-smtp
@@ -52,16 +72,13 @@ func handle_world(w http.ResponseWriter, r *http.Request) {
 	log.Println("auth")
 	// Choose auth method and set it up
 	auth := smtp.PlainAuth("", FROM, PASSWORD, "smtp.yandex.ru")
-
-	body, _ := ioutil.ReadAll(r.Body)
-
 	log.Println("prepare and send")
 	// Here we do it all: connect to our server, set up a message and send it
 	to := []string{"m.darin.comco@yandex.ru"}
 	msg := []byte("To: m.darin.comco@yandex.ru\r\n" +
 		"Subject: JIRA webhook machined\r\n" +
 		"\r\n" +
-		"BODY\r\n" + string(body) +
+		"BODY\r\n" + message +
 		"\r\n")
 	err := smtp.SendMail("smtp.yandex.ru:587", auth, FROM, to, msg)
 	if err != nil {
@@ -70,10 +87,4 @@ func handle_world(w http.ResponseWriter, r *http.Request) {
 	} else {
 		log.Println("Successfull done!")
 	}
-	fmt.Fprintf(w, "[world] webhook machined!")
-}
-
-func handle_jira_webhook(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Fprintf(w, "webhook machined!")
 }
